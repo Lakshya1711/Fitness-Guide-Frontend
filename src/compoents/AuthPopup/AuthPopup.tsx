d95fab0ee73b1ae68dede2b248d4e68431cab537
Lakshya1711 <Lucky17sh@gmail.com>
2024-01-19 22:22:18 -0800

    Started with Auth page

--- src/compoents/AuthPopup/AuthPopup.tsx
+++ src/compoents/AuthPopup/AuthPopup.tsx
@@ -1,7 +1,24 @@
 import React from "react";
-
+import './AuthPopup.css';
 const AuthPopup = () => {
-  return <div>AuthPopup</div>;
+  const [showSignup, setShowSignup] = React.useState<boolean>(false)
+  return <div className="popup">
+    {
+      showSignup ? (
+        <div>
+          <button onClick={() => {
+            setShowSignup(false)
+          }}>Login</button>
+        </div>
+      ) : (
+        <div>
+          <button onClick={() => {
+            setShowSignup(true)
+          }}>Signup</button>
+        </div>
+      )
+    }
+  </div>;
 };
 
 export default AuthPopup;
