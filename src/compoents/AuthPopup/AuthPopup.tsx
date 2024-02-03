e8cd763f146ea0bafcf7ea08e5188072800f0925
Lakshya1711 <Lucky17sh@gmail.com>
2024-02-03 23:01:34 +0530

    Signup in Progress

--- src/compoents/AuthPopup/AuthPopup.tsx
+++ src/compoents/AuthPopup/AuthPopup.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import logo from "@/assets/logo.png"
 import Input from '@mui/joy/Input';
 import { Button } from "@mui/joy";
+import Select from '@mui/joy/Select';
+import Option from '@mui/joy/Option';
 
 const AuthPopup = () => {
   const [showSignup, setShowSignup] = React.useState<boolean>(false)
@@ -13,10 +15,53 @@ const AuthPopup = () => {
   return <div className="popup">
     {
       showSignup ? (
-        <div>
-          <button onClick={() => {
-            setShowSignup(false)
-          }}>Login</button>
+        <div className="authform">
+          <div className="left">
+            <Image src={logo} alt='Logo' />
+          </div>
+          <div className="right">
+            <h1>Choose the Right Path towards Fitness</h1>
+            <form action="">
+              <Input
+                color="primary"
+                disabled={false}
+                placeholder="Email"
+                size="lg"
+                variant="soft"
+              />
+
+              <Input
+                color="primary"
+                disabled={false}
+                placeholder="Password"
+                size="lg"
+                variant="soft"
+              />
+
+              <div className="form_input_leftright">
+                <Input color="primary" size="lg" variant="outlined" type="number" placeholder="Age" />
+                <Input color="primary" size="lg" variant="outlined" type="number" placeholder="Weight" />
+              </div>
+
+              <Select
+                color="primary"
+                placeholder="Gender"
+                size="lg"
+                variant="outlined"
+              >
+                <Option value="male">Male</Option>
+                <Option value="female">Female</Option>
+                <Option value="other">Other</Option>
+              </Select>
+              <Button onClick={() => {
+                handleSignup
+              }}>Signup</Button>
+            </form>
+            <p>Already have an account ? <button onClick={() => {
+              setShowSignup(false)
+            }}>Login</button></p>
+          </div>
+
         </div>
       ) : (
         <div className="authform">
